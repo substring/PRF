b8435582651f9527958a2dbaca40e8a0bf17d156
Subs <frog2wah@gmail.com>
2022-11-16 00:48:20 +0100

    First multithreading shot

--- main.py
+++ main.py
@@ -1,20 +1,60 @@
 import argparse
+import concurrent.futures
 import os
 
+from rom import Rom
 from extlibs.pyrominfo.pyrominfo import RomInfo
 from extlibs.pyrominfo.pyrominfo import dreamcast, gameboy, gba, genericdisc, genesis, mastersystem, nes, nintendo64, nintendods, saturn, snes
-from src.loadmodule import loadModule
+
 
 """
 List the possible arguments
 """
+
 parser = argparse.ArgumentParser(description='Parse a roms folder and extract data from roms')
+parser.add_argument("path", type=str, help="A path to roms")
 parser.add_argument("--system", "-s", help="System name of the roms folder",
-     choices=['dreamcast', 'gb', 'gba', 'megadrive', 'mastersystem', 'nes', 'n64', 'nds', 'saturn', 'snes'], required=True)
+    choices=['dreamcast', 'gb', 'gba', 'megadrive', 'mastersystem', 'nes', 'n64', 'nds', 'saturn', 'snes'], required=True)
 parser.add_argument("--jobs", "-j", help="Sets the number of parallel jobs to run", type=int, default=1)
 args = parser.parse_args()
 
+def parse_rom(rom_file: str):
+    # Determine if the file is a supported archive or not
+    # Archive: make sure there is only one rom inside and buffer it
+    # Compute its hashes
+    my_rom = Rom(rom_file)
+    if my_rom.archiveContent and len(my_rom.archiveContent) == 1:
+        real_rom = list(my_rom.archiveContent[0].keys())[0]
+        rom_data = my_rom.extractRom('ram', real_rom)
+        return RomInfo.parseBuffer(rom_data)
+    # print(my_rom)
+    return RomInfo.parse(rom_file)
+
 
 if __name__ == '__main__':
-  print(RomInfo.parse('extlibs/pyrominfo/tests/data/Akumajou Dracula.fds'))
-  print(RomInfo.parse('extlibs/pyrominfo/tests/data/Sonic the Hedgehog.bin'))
+    print(RomInfo.parse('extlibs/pyrominfo/tests/data/Akumajou Dracula.fds'))
+    print(RomInfo.parse('extlibs/pyrominfo/tests/data/Sonic the Hedgehog.bin'))
+
+    if not os.path.exists(args.path):
+        raise ValueError("Path %s doesn't exist", args.path)
+
+    # find files
+    files_list = []
+    for path, subdirs, files in os.walk(args.path):
+        for name in files:
+            files_list.append(os.path.join(path, name))
+    # print(files_list)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers = args.jobs) as executor:
+        parsed_files = {executor.submit(parse_rom, file): file for file in files_list}
+        for rom in concurrent.futures.as_completed(parsed_files):
+            result = parsed_files[rom]
+            # data = rom.result()
+            # print('Parsing result: %s' % data)
+            try:
+                data = rom.result()
+            except Exception as exc:
+                print('%r generated an exception: %s' % (result, exc))
+            else:
+                print('Parsing result: %s' % data)
+
---
+++ rom.py
@@ -0,0 +1,128 @@
+import binascii
+import hashlib
+import os
+import py7zr
+import zipfile
+
+class Rom:
+	# rom must be a fullpath to an existing rom file
+	def __init__(self, rom: str, crc = '', filecrc = ''):
+		if not os.path.exists(rom):
+			raise Exception(rom + " doesn't exist")
+		self.rompathname = rom
+		self.rompath = os.path.dirname(rom)
+		self.romfile = os.path.basename(rom)
+		self.romname = os.path.splitext(self.romfile)[0]
+		self.romext = os.path.splitext(rom)[1][1:]
+		self.crc = crc
+		self.md5 = None
+		self.sha1 = None
+		self.filecrc = filecrc
+		self.archiveContent = []
+		self.isoExtensions = ['iso', 'cue', 'chd']
+		self.getCRC()
+		self.getMD5()
+		self.getSHA1()
+
+	def __repr__(self):
+		return "Rom('{}', crc = '{}', filecrc = '{}')".format(self.rompathname, self.crc, self.filecrc)
+
+	def __str__(self):
+		return "Rom: {}\nSplit into {} / {} . {}\nHashes:\n  - CRC: {}\n  - MD5: {}\n  - SHA1: {}\nFile content:: {}".format(self.rompathname, self.rompath, self.romfile, self.romext, self.crc, self.md5, self.sha1, self.archiveContent)
+
+	def getCRC(self) -> str |None:
+		if self.crc:
+			return self.crc
+		self.listArchive()
+		if len(self.archiveContent) == 1:
+			self.crc = list(self.archiveContent[0].values())[0].zfill(8)
+		else:
+			self.crc = None
+		return self.crc
+
+	def fileCRC(self) -> str:
+		buf = open(self.rompathname, 'rb').read()
+		buf = (binascii.crc32(buf) & 0xFFFFFFFF)
+		self.filecrc = "%08X" % buf
+		return self.filecrc
+
+	def listArchiveFromZip(self) -> list:
+		filesList = []
+		with zipfile.ZipFile(self.rompathname) as romzip:
+			zipinfodata = romzip.infolist()
+			for f in zipinfodata:
+				decimalCRC = romzip.getinfo(f.filename).CRC
+				filesList.append({f.filename: f'{decimalCRC:x}'})
+		return filesList
+
+	def listArchiveFrom7z(self) -> list:
+		filesList = []
+		with py7zr.SevenZipFile(self.rompathname, 'r') as romzip:
+			zipinfodata = romzip.list()
+			for f in zipinfodata:
+				decimalCRC = zipinfodata[0].crc32
+				filesList.append({f.filename: f'{decimalCRC:x}'})
+		return filesList
+
+	def listArchive(self) -> list | None:
+		if self.romext not in ['7z', 'zip']:
+			return None
+		if self.romext == 'zip':
+			self.archiveContent = self.listArchiveFromZip()
+		if self.romext == '7z':
+			self.archiveContent = self.listArchiveFrom7z()
+
+	def extractFileFromZip(self, archiveFile: str, destinationPath = '/tmp') -> str:
+		if destinationPath == 'ram':
+			return zipfile.ZipFile(self.rompathname).read(archiveFile)
+		with zipfile.ZipFile(self.rompathname) as romzip:
+			outputFile = romzip.extract(archiveFile, destinationPath)
+			return outputFile
+
+	def extractFileFrom7z(self, archiveFile: str, destinationPath = '/tmp') -> str:
+		with py7zr.SevenZipFile(self.rompathname, 'r') as romzip:
+			romzip.extract(destinationPath, archiveFile)
+			return "{}/{}".format(destinationPath, archiveFile)
+
+	def extractRom(self, path='/tmp', fileName=None):
+		extractedFileLocation = None
+		if not fileName:
+			fileName = list(self.archiveContent[0].keys())[0]
+		if self.romext == 'zip':
+			extractedFileLocation = self.extractFileFromZip(fileName, path)
+		if self.romext == '7z':
+			extractedFileLocation = self.extractFileFrom7z(fileName, path)
+		return extractedFileLocation
+
+	def md5sum(self, filename):
+		return hashlib.md5(open(filename,'rb').read()).hexdigest()
+
+	def sha1sum(self, filename):
+		return hashlib.sha1(open(filename,'rb').read()).hexdigest()
+
+	def getMD5orSHA1(self, hashType) -> str | None:
+		rom = ''
+		if hashType not in ['md5', 'sha1']:
+			return None
+		if len(self.archiveContent) != 1:
+			if hashType == 'md5':
+				return self.md5sum(self.rompathname)
+			elif hashType == 'sha1':
+				return self.sha1sum(self.rompathname)
+		rom = self.extractRom()
+		if hashType == 'md5':
+			romHash = self.md5sum(rom)
+		elif hashType == 'sha1':
+			romHash = self.sha1sum(rom)
+		os.remove(rom)
+		return romHash
+
+	def getMD5(self):
+		if not self.md5:
+			self.md5 = self.getMD5orSHA1('md5')
+		return self.md5
+
+	def getSHA1(self):
+		if not self.sha1:
+			self.sha1 = self.getMD5orSHA1('sha1')
+		return self.sha1
