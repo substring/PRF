e02697835c25a3a6e9cf8a460412aee6c595e791
Subs <frog2wah@gmail.com>
2022-11-23 14:18:48 +0000

    Slight rewrite for a better debugging

--- main.py
+++ main.py
@@ -18,7 +18,9 @@ parser.add_argument("path", type=str, help="A path to roms")
 parser.add_argument("--system", "-s", help="System name of the roms folder",
     choices=['dreamcast', 'gb', 'gba', 'megadrive', 'mastersystem', 'nes', 'n64', 'nds', 'saturn', 'snes'], required=True)
 parser.add_argument("--jobs", "-j", help="Sets the number of parallel jobs to run", type=int, default=1)
-parser.add_argument("--naming", "-n", help="Specify a rom naming convention", type=int, default=1, choices=['nointro', 'goodset'])
+parser.add_argument("--naming", "-n", help="Specify a rom naming convention (Not yet implemented)", type=int, default=1, choices=['nointro', 'goodset'])
+parser.add_argument("--fail", help="No exceptions mangagement, break on any error", action='store_true')
+parser.add_argument("--print", "-p", help="Print a light report at the end", action='store_true')
 args = parser.parse_args()
 
 def parse_rom(rom_file: str):
@@ -37,6 +39,7 @@ def parse_rom(rom_file: str):
         rom_data = my_rom.extractRom()
         ret = RomInfo.parseBuffer(rom_data)
     else:
+        #print(rom_file)
         ret = RomInfo.parse(rom_file)
 
     if not ret:
@@ -62,6 +65,17 @@ def clean_name_goodset(rom_obj: Rom) -> str|None:
 
 def clean_name_nointro(name: Rom) -> str|None:
     pass
+
+def final_output(ok_roms_list: {}):
+    for rom in ok_roms_list:
+        #print(rom)
+        #continue
+        print(rom['cleaned_title'], end='')
+        if 'title' in rom:
+            print(" - %s" % rom['title'], end='')
+        if 'foreign_title' in rom:
+            print(" / %s" % rom['foreign_title'], end='')
+        print()
 
 if __name__ == '__main__':
     #print(RomInfo.parse('extlibs/pyrominfo/tests/data/Akumajou Dracula.fds'))
@@ -89,6 +103,9 @@ if __name__ == '__main__':
         for rom in concurrent.futures.as_completed(parsed_files):
             result = parsed_files[rom]
             nb_parsed_files += 1
+            if args.fail:
+                data = rom.result()
+                continue
             try:
                 data = rom.result()
             except Exception as exc:
@@ -103,10 +120,5 @@ if __name__ == '__main__':
     print() # bring back a \n
     print("Roms not parsed: %d/%d" % (len(roms_error), total_nb_files))
 
-    for rom in roms_ok:
-        print(rom['cleaned_title'], end='')
-        if 'title' in rom:
-            print(" - %s" % rom['title'], end='')
-        if 'foreign_title' in rom:
-            print(" / %s" % rom['foreign_title'], end='')
-        print()
+    if args.print:
+        final_output(roms_ok)
